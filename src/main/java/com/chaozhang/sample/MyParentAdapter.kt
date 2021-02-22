bb1e4569834c11ce10cf45ea8037215985dab9e4
Chao Zhang <cazhang@linkedin.com>
2021-02-22 10:33:54 -0800

    Solution: @JvmSuppressWildcards

--- src/main/java/com/chaozhang/sample/MyParentAdapter.kt
+++ src/main/java/com/chaozhang/sample/MyParentAdapter.kt
@@ -4,21 +4,21 @@ open class MyParentAdapter : RecyclerViewAdapter() {
 
     override fun onBindViewHolder(
         position: Int,
-        payloads: List<Any>
+        payloads: List<@JvmSuppressWildcards Any>
     ) { }
 
     override fun onBindViewHolder2(
         position: Int,
-        payloads: List<Any?>
+        payloads: List<@JvmSuppressWildcards Any?>
     ) { }
 
     override fun onBindViewHolder3(
         position: Int,
-        payloads: List<Any>
+        payloads: List<@JvmSuppressWildcards Any>
     ) { }
 
     override fun onBindViewHolder4(
         position: Int,
-        payloads: List<Any>
+        payloads: List<@JvmSuppressWildcards Any>
     ) { }
 }
